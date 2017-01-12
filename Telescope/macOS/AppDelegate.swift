3c5a6f7f5f39172e41a6bf0382d1a40a8783fdf9
Todd Olsen <todd.olsen@me.com>
2017-01-11 19:43:41 -0500

    Created macOS project.

---
+++ Telescope/macOS/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Telescope_macOS
+//
+//  Created by Todd Olsen on 1/11/17.
+//
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
